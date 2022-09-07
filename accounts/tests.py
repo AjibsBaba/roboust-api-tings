51e3d3a962e33dce5e3af02d4d7b0496c6e26199
AjibsBaba <samuelajibade22@gmail.com>
2022-09-07 10:57:21 +0100

    created user model

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,22 @@
+import uuid
+
 from django.test import TestCase
 
-# Create your tests here.
+from accounts.models import User
+
+
+class AccountTest(TestCase):
+    def setUp(self):
+        User.objects.create(id=uuid.uuid4(), email='samuel@robustapi.com', first_name='Samuel', last_name='Ajibade',
+                            is_active=True, is_superuser=False)
+        User.objects.create(id=uuid.uuid4(), email='kunle@robustapi.com', first_name='Kunle', last_name='Afod',
+                            is_active=True, is_superuser=False)
+
+    def test_users_have_emails(self):
+        """
+        Users are correctly identified with their email addresses
+        """
+        samuel = User.objects.get(email='samuel@robustapi.com')
+        kunle = User.objects.get(email='kunle@robustapi.com')
+        self.assertEqual(samuel.has_email(), 'samuel@robustapi.com')
+        self.assertEqual(kunle.has_email(), 'kunle@robustapi.com')
