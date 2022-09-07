d19075000b0d0908a3e7fad17ed086f748604660
AjibsBaba <samuelajibade22@gmail.com>
2022-09-07 10:27:17 +0100

    created custom user manager

---
+++ accounts/managers.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Creates and Stores a User Account
+        :param email:
+        :param password:
+        :param extra_fields:
+        :return: user
+        """
+        if not email:
+            raise ValueError("A valid email address is required")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('is_superuser must be set to True.')
+        return self._create_user(email, password, **extra_fields)
