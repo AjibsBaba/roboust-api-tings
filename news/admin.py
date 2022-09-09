5a19bf67a1e8844bc6c16d5e825d8fe5cc6e96d4
AjibsBaba <samuelajibade22@gmail.com>
2022-09-09 17:39:34 +0100

    created models for articles and authors added admin edit support for models added custom filters

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from news.models import Author, Article
 
-# Register your models here.
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'date_joined')
+    list_filter = ('date_joined',)
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('author', 'title', 'description', 'date_published')
+    list_filter = ('author', 'date_published')
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Article, ArticleAdmin)
--- news/models.py
+++ news/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Author(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, verbose_name='Authors ID')
+    email = models.EmailField(verbose_name='Email Address', unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    avatar = models.ImageField(upload_to='Avatars/', width_field=300, height_field=300)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+
+class Article(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=150)
+    content = models.CharField(max_length=350)
+    date_published = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='Articles/')
+
